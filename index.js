c43a01dd83e5565422e9427fb6ec0dc90d21b20c
badc0ffee0ddf00d <badc0ffee0ddf00d@users.noreply.github.com>
2016-02-13 00:24:51 +0100

    test js

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ $(function(){
       
       
       
-       $.getJSON("URL: https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=31d8dc448768d1b40ccf61cae19d7a50&per_page=12&format=json&nojsoncallback=1&api_sig=378e5a7caf3683af2e1692f4b55acb3c",
+       $.getJSON("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=31d8dc448768d1b40ccf61cae19d7a50&per_page=12&nojsoncallback=1&api_sig=378e5a7caf3683af2e1692f4b55acb3c",
         {
 			
-            text: $("#srch-term").val()
-            //format: "json"
+            text: $("#srch-term").val(),
+            format: "json"
             
         }, function( data ) {
       
