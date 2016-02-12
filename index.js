a85548e8362d1734044e4d7843484553861a37fb
badc0ffee0ddf00d <badc0ffee0ddf00d@users.noreply.github.com>
2016-02-12 23:22:38 +0100

    js tests

---
+++ index.js
@@ -0,0 +1,16 @@
+$(document).ready({
+
+   $('#contactForm').submit(function() {
+
+      //Do stuff here
+
+   }); 
+
+});
+
+
+ $(window).scroll(function () {
+    if ($(document).height() <= $(window).scrollTop() + $(window).height()) {
+        alert("End Of The Page");
+    }
+ });
