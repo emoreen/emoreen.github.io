0a3c953fef10293e190155eb7472537292ab63fd
badc0ffee0ddf00d <badc0ffee0ddf00d@users.noreply.github.com>
2016-02-13 00:22:41 +0100

    test js

--- index.js
+++ index.js
@@ -8,6 +8,20 @@ $(function(){
       
       
       
+       $.getJSON("URL: https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=31d8dc448768d1b40ccf61cae19d7a50&per_page=12&format=json&nojsoncallback=1&api_sig=378e5a7caf3683af2e1692f4b55acb3c",
+        {
+			
+            text: $("#srch-term").val()
+            //format: "json"
+            
+        }, function( data ) {
+      
+      
+      
+      
+      
+       });
+      
       
       
       
