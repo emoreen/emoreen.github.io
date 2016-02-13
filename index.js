0e3c7e59404da3164ec19c495caec19219432877
badc0ffee0ddf00d <badc0ffee0ddf00d@users.noreply.github.com>
2016-02-13 01:06:23 +0100

    test js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ $(function(){
       
       
       
-      $("<div/>").attr("class", "col-md-4").appendTo(".row").append($("<img/>").attr("src", "https://farm"+item.farm+".staticflickr.com/"+item.server+"/"+item.id+"_"+item.secret+".jpg").wrap("<a href=" + item.id + "></a>"));
+      $("<div/>").attr("class", "col-md-4").appendTo(".row").append("<p>lol</p>");
       
       
 		//$("<img/>").attr("src", "https://farm"+item.farm+".staticflickr.com/"+item.server+"/"+item.id+"_"+item.secret+".jpg").wrap("<a href=" + item.id + "></a>");
