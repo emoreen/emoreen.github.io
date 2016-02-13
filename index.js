b62bbc34c75ea595a7c71f352247f510b808654a
emoreen <erikmw@msn.com>
2016-02-13 16:56:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ $(function(){
       
       
       
-      $("<div/>").attr("class", "col-md-4").append($("<a/>").attr("href", item.id).append($("<img/>").attr("src", "https://farm"+item.farm+".staticflickr.com/"+item.server+"/"+item.id+"_"+item.secret+"_c.jpg").attr("class", "img-responsive"))).appendTo(".row");
+      $("<div/>").attr("class", "testcol-md-4").append($("<a/>").attr("href", item.id).append($("<img/>").attr("src", "https://farm"+item.farm+".staticflickr.com/"+item.server+"/"+item.id+"_"+item.secret+"_c.jpg").attr("class", "img-responsive"))).appendTo(".row");
       
       
 		//$("<img/>").attr("src", "https://farm"+item.farm+".staticflickr.com/"+item.server+"/"+item.id+"_"+item.secret+".jpg").wrap("<a href=" + item.id + "></a>");
@@ -73,7 +73,7 @@ $(window).scroll(function () {
       
       
       
-      $("<div/>").attr("class", "col-md-4").append($("<a/>").attr("href", item.id).append($("<img/>").attr("src", "https://farm"+item.farm+".staticflickr.com/"+item.server+"/"+item.id+"_"+item.secret+"_c.jpg").attr("class", "img-responsive"))).appendTo(".row");
+      $("<div/>").attr("class", "testcol-md-4").append($("<a/>").attr("href", item.id).append($("<img/>").attr("src", "https://farm"+item.farm+".staticflickr.com/"+item.server+"/"+item.id+"_"+item.secret+"_c.jpg").attr("class", "img-responsive"))).appendTo(".row");
       
       
  
