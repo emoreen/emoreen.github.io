d05ac7f757c8a8548c18534aef107b2a4855da4c
jonas-g <jonas.gabrielson@me.com>
2016-02-14 16:27:12 +0100

    More indent cleanup

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 $(function(){
 
 	$("#mainsearch").submit(function () {
-    	$(".img-responsive").remove();
+		$(".img-responsive").remove();
 		$(".col-md-12").remove();
-      
+
 		$.getJSON("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=ae34bd27a7623c8315fbe0cc73cd9ca6&per_page=12&sort=relevance&nojsoncallback=1", {
 			text: $("#srch-term").val(),
-            format: "json"
-        }, function( data ) {
+			format: "json"
+		}, function( data ) {
 			$.each(data.photos.photo, function(i,item){
 				$("<a/>").attr("href", item.id).append($("<img/>").attr("src", "https://farm"+item.farm+".staticflickr.com/"+item.server+"/"+item.id+"_"+item.secret+"_c.jpg").attr("class", "img-responsive")).appendTo(".row");
 			});
@@ -20,14 +20,14 @@ $(function(){
 	var page = 2;
 	
 	$(window).scroll(function () {
-	    if ($(document).height() <= $(window).scrollTop() + $(window).height()) {
+		if ($(document).height() <= $(window).scrollTop() + $(window).height()) {
 			$("<div/>").attr("id", "pagerow"+page).attr("class", "row").appendTo("#maincont");
 			var currentrow = "#pagerow" + page;
 	
 			$.getJSON("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=ae34bd27a7623c8315fbe0cc73cd9ca6&per_page=12&sort=relevance&nojsoncallback=1", {
-	            text: $("#srch-term").val(),
-	            format: "json",
-	            page: page
+				text: $("#srch-term").val(),
+				format: "json",
+				page: page
 			}, function( data ) {
 				$.each(data.photos.photo, function(i,item){
 					$("<a/>").attr("href", item.id).append($("<img/>").attr("src", "https://farm"+item.farm+".staticflickr.com/"+item.server+"/"+item.id+"_"+item.secret+"_c.jpg").attr("class", "img-responsive")).appendTo( currentrow );
